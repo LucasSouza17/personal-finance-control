bcdc36055c831283d24b25e8a9403dd86376c0fb
Lucas Fernando de Souza <lucasnando800@hotmail.com>
2021-12-03 00:30:33 -0300

    Criação de componentes e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { DashBoard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <DashBoard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
